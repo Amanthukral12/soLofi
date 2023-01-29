99acc5c6c452db1962b953b0417368591032e9f7
amanthukral12 <thukral618@gmail.com>
2023-01-29 23:18:51 +0530

    Add song data

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+function musicData() {
+  return [
+    {
+      index: 0,
+      id: "Qt0-9mO-ZXY",
+      name: "Space Lofi Hip Hop Radio 24/7 🚀",
+    },
+    {
+      index: 1,
+      id: "7NOSDKb0HlU",
+      name: "lofi hip hop radio - beats to study/relax to 🐾 ",
+    },
+    {
+      index: 2,
+      id: "5yx6BWlEVcY",
+      name: "Chillhop Radio - jazzy & lofi hip hop beats 🐻",
+    },
+    {
+      index: 3,
+      id: "MCkTebktHVc",
+      name: "College Music - 24/7 lofi hip hop radio",
+    },
+    {
+      index: 4,
+      id: "VNTm0tVqk3w",
+      name: "New York Coffee Shop Ambience ☕",
+    },
+    {
+      index: 5,
+      id: "eyGPIpZ7208",
+      name: "🔴 Relaxing Music for Studying 24/7, Ocean Wave Sounds",
+    },
+  ];
+}
