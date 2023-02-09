b59931c77ffdc94aba5cf5c17d4efe24543fda99
amanthukral12 <thukral618@gmail.com>
2023-02-10 01:15:06 +0530

    Add Navbar with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import MusicPlayer from "./components/MusicPlayer";
-import RandomGif from "./components/RandomGif";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      SoLofi
+      <Navbar />
       <MusicPlayer />
     </div>
   );
