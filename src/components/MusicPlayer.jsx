b89f664ba3ceb1cd8615471d925274ea58137af5
Aman Thukral <thukral618@gmail.com>
2023-03-23 23:42:00 +0530

    Complete Design Change

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -11,6 +11,7 @@ import {
   BiVolumeMute,
 } from "react-icons/bi";
 import "../styles/music.css";
+import Navbar from "./Navbar";
 const MusicPlayer = () => {
   const {
     currentSong,
@@ -20,47 +21,50 @@ const MusicPlayer = () => {
     pause,
     volume,
     setVolume,
+    data,
   } = useContext(Music);
 
   const handleVolume = (e) => {
-    console.log(parseFloat(e.target.value));
     setVolume(parseFloat(e.target.value));
   };
   return (
     <div className="app">
+      <Navbar />
       <div className="parent">
-        <RandomGif />
-        <YTWrapper />
-      </div>
-      <span className="songName">{currentSong.name}</span>
-      <div className="buttonContainer">
-        <div className="buttonContainerLeft">
-          <div className="icon" onClick={() => prevVideo()}>
-            <BiSkipPrevious />
-          </div>
-          <div className="icon" onClick={() => playPause()}>
-            {pause ? <BiPlayCircle /> : <BiPauseCircle />}
-          </div>
-          <div className="icon" onClick={() => nextVideo()}>
-            <BiSkipNext />
-          </div>
+        <div className="songImage">
+          <RandomGif />
+          <YTWrapper />
         </div>
-        <div className="volume">
-          <div
-            className="volumeIcon"
-            onClick={() => setVolume(volume === 0 ? 1 : 0)}
-          >
-            {volume === 0 ? <BiVolumeMute /> : <BiVolumeFull />}
+        <div className="buttonContainer">
+          <div className="buttonContainerLeft">{currentSong.name}</div>
+          <div className="buttonContainerMiddle">
+            <div className="icon" onClick={() => prevVideo()}>
+              <BiSkipPrevious />
+            </div>
+            <div className="icon" onClick={() => playPause()}>
+              {pause ? <BiPlayCircle /> : <BiPauseCircle />}
+            </div>
+            <div className="icon" onClick={() => nextVideo()}>
+              <BiSkipNext />
+            </div>
           </div>
-          <input
-            type="range"
-            value={volume}
-            min={0}
-            max={1}
-            step={0.01}
-            onChange={handleVolume}
-            className="slider"
-          />
+          <div className="volume">
+            <div
+              className="volumeIcon"
+              onClick={() => setVolume(volume === 0 ? 1 : 0)}
+            >
+              {volume === 0 ? <BiVolumeMute /> : <BiVolumeFull />}
+            </div>
+            <input
+              type="range"
+              value={volume}
+              min={0}
+              max={1}
+              step={0.01}
+              onChange={handleVolume}
+              className="slider"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/RandomGif.jsx
+++ src/components/RandomGif.jsx
@@ -4,7 +4,7 @@ import "../styles/music.css";
 const RandomGif = () => {
   const { gif, setGif } = useContext(RandomGifs);
   const getRandomGif = () => {
-    const gifs = ["1", "2", "3", "4"];
+    const gifs = ["1", "2", "3", "4", "5"];
     var a = Math.floor(Math.random() * gifs.length + 1);
     var src = `../assets/gifs/${a}.gif`;
     setGif(src);
